29ab607359034969f70ddf4ba91a8c1a7ba71891
MarceloJoel <marcelorodriguez0369@gmail.com>
2025-12-26 14:16:23 -0500

    fead: add UsuarioService class

--- src/main/java/com/hibernate/ferreteria/servicios/UsuarioService.java
+++ src/main/java/com/hibernate/ferreteria/servicios/UsuarioService.java
@@ -1,5 +1,30 @@
 package com.hibernate.ferreteria.servicios;
+import com.hibernate.ferreteria.repositorios.Repo_usuarios;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-public class UsuarioService {
+import java.util.List;
 
+@Service
+public class UsuarioService implements UserDetailsService {
+
+    @Autowired
+    private Repo_usuarios repoUsuarios;
+
+    @Override
+    public UserDetails loadUserByUsername(String nombreUsuario)
+                throws UsernameNotFoundException{
+            var usuario = repoUsuarios.findByUsername(nombreUsuario)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado" + usuario));
+
+                return new User(usuario.getUsuario(), usuario.getPassword(),
+                    List.of(new SimpleGrantedAuthority("Role_" + usuario.getRol())));
+        
+    }
+    
 }
