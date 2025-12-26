a5c0a4762be30affd46d95c84f71f77b97b4b7f3
MarceloJoel <marcelorodriguez0369@gmail.com>
2025-12-26 14:00:14 -0500

    feat: add 'usuario' class

---
+++ src/main/java/com/hibernate/ferreteria/entity/Usuario.java
@@ -0,0 +1,48 @@
+package com.hibernate.ferreteria.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String usuario;
+
+    @Column(nullable = false, length = 60)
+    private String rol;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+}
